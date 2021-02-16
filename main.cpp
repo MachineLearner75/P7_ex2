ab75ad165c0c097de6e5d769fa7a186aab4a7187
Hugo SAILHEN <b00746387@essec.edu>
2021-02-16 20:53:50 +0000

    dd

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
 
+template <typename T> 
+class StackI {
+  public: 
+    virtual void push(T t) = 0;
+    virtual T pop() = 0;
+    virtual T peek() = 0;
+    virtual void print() = 0;
+};
+
+template <typename T>
+class Stack : public StackI <T> {
+
+    T *ptr;
+    int size;
+
+  public:
+    Stack<T>();
+    Stack<T>(int n);
+    void push(T t);
+    void pop();
+    T top();
+    bool isEmpty();
+    void print();
+    T *get_ptr();
+
+    int get_size();
+
+    int getNonEmpty();
+
+    ~Stack() {
+      delete [] ptr;
+    }
+};
 
 
 template <class T> Stack<T>::Stack(){
@@ -8,19 +41,24 @@ template <class T> Stack<T>::Stack(){
     size = 20;
 }
 
-template <class T> Stack<T>::Stack(int n){
+template <class T> 
+Stack<T>::Stack(int n){
     ptr = new T[n];
     size = n;
 }
 
-template <class T> T* Stack<T>::get_ptr(){
+template <class T> 
+T* Stack<T>::get_ptr(){
     return ptr;
 }
 
-template <class T> int Stack<T>::get_size(){
+template <class T> 
+int Stack<T>::get_size(){
     return size;
 }
-template <class T> int Stack<T>::getNonEmpty() {
+template <class T> 
+int Stack<T>::getNonEmpty() {
+
     int noempt = 0;
     for (int i = 0; i < size; i++) {
         if (ptr[i] != NULL) {
@@ -31,6 +69,7 @@ template <class T> int Stack<T>::getNonEmpty() {
 }
 
 template <class T> void Stack<T>::push(T t){
+  
     int noempt=getNonEmpty();
         if(noempt!=size){
             ptr[size-1-noempt]=t;
@@ -70,5 +109,22 @@ template <class T> void Stack<T>::print(){
         for(int i=size-getNonEmpty() ;i <size;i++){
             std::cout<<ptr[i]<<";";
         }
+    }
+}
+
+int main() {
+    Stack <int> a(12);
+    for(int i=1;i<a.get_size()+1;i++){
+        a.push(i);
+    }
+    a.print();
+    std::cout<<"\n";
+    a.pop();
+    a.pop();
+    std::cout<<a.top()<<"\n";
+    for(int i=0;i<11;i++){
+        a.pop();
+        a.print();
+        std::cout<<"\n";
     }
 }
